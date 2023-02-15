30cae9fc9ed77e30d2a179cf11a3a13283713bdb
Marcos <marcos.vinicius.dev.2004@gmail.com>
2023-02-15 10:43:07 -0300

    Criando cabeçalho do app

---
+++ src/mainClass.java
@@ -0,0 +1,10 @@
+public class mainClass {
+    public static void main(String[] args) throws Exception {
+        /*
+         * Cabeçalho
+         */
+        System.out.println("---------------------------------------------");
+        System.out.println("|       CALCULADORA DE NÚMEROS PRIMOS       |");
+        System.out.println("---------------------------------------------");
+    }
+}
